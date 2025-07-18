9b1e481f74a9ec0ac9107bd2d1308ed0f34a076c
fghyj4er234 <urazzaq714@gmail.com>
2025-07-18 09:57:14 +0500

    Variables related programs

---
+++ Variables.py
@@ -0,0 +1,47 @@
+#Code:1 = Assign a number to a variable x. Then assign another number to y. Print their sum
+
+x = 20
+y = 10.5
+
+sum = x+y
+print(sum)
+
+#Code:2 = Create two variables a = 10 and b = 3. Swap their values using a third variable.
+
+a = 10
+b = 3
+#Swap
+temp = a
+a = b
+b = temp
+
+print("a =" ,a)
+print("b =" ,b)
+
+#Code:3 = Create a variable radius = 5. Calculate the area using formula area = pi * radius * radius.
+
+radius = 5 
+#We know value of Pi
+pi = 3.14
+
+area = pi * radius * radius
+
+print("area = " , area)
+
+
+#Code:4 = Assign your age to a variable my_age. Create another variable friend_age and find the average.
+
+my_age = 20
+friend_age = 18
+
+average_age = (my_age+friend_age)/2
+
+print(f"Average age is : {average_age}")
+
+
+#Code:5 = Create a variable x = 7. Without using any new variables, double the value of x.
+
+x = 7
+x = 7 * 2
+
+print("x =",x)
